f31f288d14d009969f2c2bb7081e103af713bb04
Kerem Yıldırım <keremyldrm1670@gmail.com>
2026-04-12 16:53:14 +0300

    feat: added user registration with bcrypt password hashing

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -22,4 +22,11 @@ const usersSchema = new Schema(
   },
 );
 
+// Kullanıcı verisi JSON'a dönüştürüldüğünde şifreyi otomatik olarak kaldırır
+usersSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 export const UsersCollection = model('users', usersSchema);
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,13 @@
+import bcrypt from 'bcrypt';
+
 import { UsersCollection } from '../db/models/user.js';
 
 // Kullanıcı kaydı için registerUser fonksiyonu
 export const registerUser = async (payload) => {
-  return await UsersCollection.create(payload);
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  return await UsersCollection.create({
+    ...payload,
+    password: encryptedPassword,
+  });
 };
